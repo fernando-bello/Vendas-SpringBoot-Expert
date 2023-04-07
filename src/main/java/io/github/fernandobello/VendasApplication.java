fe6ca1a2bd3af5d8207aef50e5b9e8da07aa40d3
Fernando Bello <fernandobello700220@gmail.com>
2023-04-07 10:03:41 -0300

    Primeiro Hello World em Spring

--- src/main/java/io/github/fernandobello/VendasApplication.java
+++ src/main/java/io/github/fernandobello/VendasApplication.java
@@ -2,9 +2,18 @@ package io.github.fernandobello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication //O Spring reconhece que essa classe vai inicializar uma aplicação Spring Boot
+@RestController //Essa classe será o controlador rest (posso controlar e mandar mensagens ao Browser)
 public class VendasApplication {
+
+    @GetMapping("/hello")
+    public String helloWorld() {
+        return "hello world";
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
     }
