5f3287c644e062651738523ed70223e6d2973dfd
Fernando Bello <fernandobello700220@gmail.com>
2023-04-16 10:55:31 -0300

    Criadas classes model

---
+++ src/main/java/io/github/fernandobello/domain/entity/Cliente.java
@@ -0,0 +1,27 @@
+package io.github.fernandobello.domain.entity;
+
+public class Cliente {
+    private Integer id;
+    private String nome;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Cliente(Integer id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+}
