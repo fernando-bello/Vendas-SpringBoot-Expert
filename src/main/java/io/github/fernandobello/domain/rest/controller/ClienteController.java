b9c1bca782f356675588382a3ed8cb3bed2c30fd
Fernando Bello <fernandobello700220@gmail.com>
2023-05-07 08:45:06 -0300

    Adicionadas funções save e delete ao controlador REST de clientes

--- src/main/java/io/github/fernandobello/domain/rest/controller/ClienteController.java
+++ src/main/java/io/github/fernandobello/domain/rest/controller/ClienteController.java
@@ -5,10 +5,7 @@ import io.github.fernandobello.domain.entity.Cliente;
 import io.github.fernandobello.domain.repository.ClientesRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -31,8 +28,25 @@ public class ClienteController {
             return ResponseEntity.ok(cliente.get());
         }
         return ResponseEntity.notFound().build();
+    }
 
+    @PostMapping("/api/clientes")
+    @ResponseBody
+    public ResponseEntity save(@RequestBody Cliente cliente) {
+        Cliente clienteSalvo = clientes.save(cliente);
+        return ResponseEntity.ok(clienteSalvo);
+    }
 
+    @DeleteMapping("api/clientes/{id}")
+    @ResponseBody
+    public ResponseEntity delete(@PathVariable Integer id) {
+        Optional<Cliente> cliente = clientes.findById(id);
+
+        if (cliente.isPresent()) {
+            clientes.delete(cliente.get());
+            return ResponseEntity.noContent().build(); //noContent() é um codigo de status de sucesso, que não retorna NADA
+        }
+        return ResponseEntity.notFound().build();
     }
 }
 
